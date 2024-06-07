735845e313f0e81a712d6f06814114234b554771
shunpei12345321 <u-shunpei-2404@occ.co.jp>
2024-06-07 10:24:04 +0900

    111

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,13 +3,12 @@ import prisma from "@/lib/PrismaClient";
 
 export const POST = async (req: Request, res: NextResponse) => {
 	const { name, email, fax, age } = await req.json();
-	const ageInt = parseInt(age, 10);
 	const new_user = await prisma.user.create({
 		data: {
 			name,
 			email,
 			fax,
-			age: ageInt,
+			age,
 		},
 	});
 	return NextResponse.json(new_user);
